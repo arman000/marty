54957b5268a68cefd7f8ecde04b88c25deab1685
Arman Bostani <abostani@pnmac.com>
2013-08-12 13:29:33 -0700

    ENTER in login now performs submit; Login name field is focused when login window is displayed;

--- app/components/marty/auth_app/javascripts/auth_app.js
+++ app/components/marty/auth_app/javascripts/auth_app.js
@@ -2,62 +2,90 @@
     onSignIn: function() {
 	var me = this;
 	this.signinWin = this.signinWin || Ext.create('widget.window', {
-	    width: 300, height: 150, modal: true, layout: 'fit',
+	    width: 	300,
+	    height: 	150,
+	    modal: 	true,
+	    layout: 	'fit',
+
+	    submit: function() {
+		var form 	= this.items.first();
+		var values 	= form.getForm().getValues();
+
+		// calling the endpoint
+		me.signIn(values, function(res) {
+		    if (res) {
+			this.signinWin.close();
+			Ext.Msg.show({
+			    title: 	"Signed in",
+			    msg: 	"Signed in successfully, reloading...",
+			    icon: 	Ext.Msg.INFO,
+			    closable: 	false
+			});
+			window.location = window.location;
+		    }
+		});
+	    },
+
 	    closeAction: 'hide',
 	    title: "Sign in",
 	    fbar: [
-		{text: 'OK', handler: function() {
-		    var form = this.up('window').items.first(),
-		    values = form.getForm().getValues();
-
-		    // calling the endpoint
-		    me.signIn(values, function(res){
-			if (res) {
-			    this.signinWin.close();
-			    Ext.Msg.show({
-				title: "Signed in",
-				msg: "Signed in successfully, reloading...",
-				icon: Ext.Msg.INFO,
-				closable: false
-			    });
-			    window.location = window.location;
-			}
-		    });
-		}},
-		{text:'Cancel', handler: function() {
-		    this.up('window').close();
-		}}
+		{
+		    text: 'OK',
+		    name: 'signin_submit',
+		    handler: function() {
+			this.up('window').submit();
+		    },
+		},
+		{
+		    text:'Cancel',
+		    handler: function() {
+			this.up('window').close();
+		    },
+		},
 	    ],
 
 	    items: {
-		xtype: 'form', bodyPadding: 10,
-		defaultType: 'textfield',
+		xtype: 		'form',
+		defaultType: 	'textfield',
+		bodyPadding: 	10,
+		defaults: {
+		    listeners: {
+			specialkey: function (field, event) {
+			    if (event.getKey() == event.ENTER) {
+				field.up('window').submit();
+			    }
+			}
+		    },
+		},
 		items: [
 		    {
-			xtype: 'displayfield'
-		    },{
-			fieldLabel: 'Login',
-			name: 'login',
-		    },{
-			fieldLabel: 'Password',
-			name: 'password',
-			inputType: 'password',
+			xtype: 		'displayfield'
+		    },
+		    {
+			fieldLabel: 	'Login',
+			name: 		'login',
+		    },
+		    {
+			fieldLabel: 	'Password',
+			name: 		'password',
+			inputType: 	'password',
 		    }
 		]
 	    }
 	});
 
 	this.signinWin.show();
+	this.signinWin.down('textfield').focus(false, 100);
     },
 
     onSignOut: function() {
 	this.signOut(null, function(success) {
 	    if (success) {
 		Ext.Msg.show({
-		    title: "Signed out",
-		    msg: "Signed out, reloading the application...",
-		    icon: Ext.Msg.INFO,
-		    closable: false
+		    title: 	"Signed out",
+		    msg: 	"Signed out, reloading the application...",
+		    icon: 	Ext.Msg.INFO,
+		    closable: 	false
 		});
 		window.location = window.location;
 	    }
