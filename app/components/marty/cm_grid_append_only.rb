c0c7ab2835e8698b36ddba423d68158daa7cc217
Eric Litwin <eric.litwin@pnmac.com>
2013-10-07 12:52:29 -0700

    Fixed bug with edit in form showing on append only tables

--- app/components/marty/cm_grid_append_only.rb
+++ app/components/marty/cm_grid_append_only.rb
@@ -3,7 +3,6 @@ class Marty::CmGridAppendOnly < Marty::McflyGridPanel
     super
 
     c.enable_extended_search	= false
-    c.enable_edit_in_form	= true
     c.prohibit_update		= true
     c.prohibit_delete		= true
   end
