ee43d0c9c1bda6f0741e23e65c1717504254115d
Eric Litwin <eric.litwin@pnmac.com>
2013-10-10 12:25:06 -0700

    Fixed sorting issue for associated columns

--- app/components/marty/mcfly_grid_panel.rb
+++ app/components/marty/mcfly_grid_panel.rb
@@ -38,7 +38,6 @@ class Marty::McflyGridPanel < Marty::CmGridPanel
     # Set mcfly_scope if the attribute is a mcfly association
     if !c[:scope] && data_adapter.association_attr?(name)
       assoc_name, assoc_method = name.split('__')
-
       begin
         aklass = data_class.reflect_on_association(assoc_name.to_sym).klass
       rescue
@@ -48,22 +47,33 @@ class Marty::McflyGridPanel < Marty::CmGridPanel
       # FIXME: MCFLY_UNIQUENESS is the easiest way I can think of
       # figuring out if a class is Mcfly.
       if (aklass.const_get(:MCFLY_UNIQUENESS) rescue nil)
-        c[:scope] = self.class.mcfly_scope
-
-        # FIXME: the following is to bypass a Netzke bug in 0.8.4.
-        # They're mistakenly setting :scopes (plural) in
-        # default_fields_for_forms().  This is fixed as of 0.9.x.
-        c[:editor] ||= {}
-        c[:editor][:scope] = self.class.mcfly_scope
+        c[:scope] = self.class.mcfly_scope(assoc_method || 'id')
+      else
+        c[:scope] = self.class.sorted_scope(assoc_method || 'id')
       end
+
+      # NOTE - Monkey patching of this method eliminates the need to do this
+      # The following is to bypass a Netzke bug in 0.8.4.
+      # They're mistakenly setting :scopes (plural) in
+      # default_fields_for_forms().  This is fixed as of 0.9.x.
+      #c[:editor] ||= {}
+      #c[:editor][:scope] = c[:scope]
     end
   end
 
-  def self.mcfly_scope
+private
+  def self.mcfly_scope(sort_column)
     lambda { |r|
       t = Marty::Util.get_posting_time
       ts = (t == Float::INFINITY) ? 'infinity' : t
-      r.where("obsoleted_dt >= ? AND created_dt < ?", ts, ts)
+      r.where("obsoleted_dt >= ? AND created_dt < ?", ts, ts).
+      order(sort_column.to_sym)
+    }
+  end
+
+  def self.sorted_scope(sort_column)
+    lambda { |r|
+      r.order(sort_column.to_sym)
     }
   end
 end
