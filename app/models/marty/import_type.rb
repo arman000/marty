d5960e088daaf908a498ae84d2a46cbb3cd7381d
Richard Rafaj <richard.rafaj@pnmac.com>
2014-07-10 13:37:01 -0700

    R58983 - Marty support for importing data via delayed job

--- app/models/marty/import_type.rb
+++ app/models/marty/import_type.rb
@@ -28,4 +28,12 @@ class Marty::ImportType < Marty::Base
     model_name.constantize
   end
 
+  delorean_fn :lookup, sig: 1 do
+    |name|
+    self.find_by_name(name)
+  end
+
+  delorean_fn :get_all, sig: 0 do
+    self.all
+  end
 end
