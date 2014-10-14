0ed572d780740ca521a3b0bbfce924cf54e15f4f
Arman Bostani <abostani@pnmac.com>
2014-10-14 09:02:23 -0700

    Bumped version to 0.2.0; Put back commented out protect_from_forgery;

--- app/controllers/marty/application_controller.rb
+++ app/controllers/marty/application_controller.rb
@@ -1,6 +1,5 @@
 class Marty::ApplicationController < ActionController::Base
-  # HACKKKKKKKKKKKKKKKKKKKKKKKKKKKK!!!
-  # protect_from_forgery
+  protect_from_forgery
 
   # Marty's ApplicationController is based on Redmine's
   # implementation.
