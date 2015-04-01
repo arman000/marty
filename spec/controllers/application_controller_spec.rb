102641cbc2654c5749d68918c4ea9e48f037cc89
Brian VanLoo <brian.vanloo@pnmac.com>
2015-04-01 15:59:36 -0700

    Add application_controller spec

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+module Marty
+  describe ApplicationController do
+    before(:each) do
+      create_gemini_user
+      subject.logout_user
+      expect(Marty::User.current).to be_nil
+    end
+
+    describe 'authentication' do
+      it "should allow a registered user to log in" do
+        Rails.configuration.marty.stub(:auth_source).and_return('local')
+
+        user = Marty::User.try_to_login('gemini', 'gemini')
+        subject.set_user(user)
+        expect(Marty::User.current).to_not be_nil
+
+        subject.logout_user
+        expect(Marty::User.current).to be_nil
+      end
+
+      it "should allow a registered user to log in when the database " +
+        "is in recovery mode" do
+        Marty::Util.stub(:db_in_recovery?).and_return(true)
+        Rails.configuration.marty.stub(:auth_source).and_return('local')
+
+        user = Marty::User.try_to_login('gemini', 'gemini')
+        subject.set_user(user)
+        expect(Marty::User.current).to_not be_nil
+
+        subject.logout_user
+        expect(Marty::User.current).to be_nil
+      end
+
+      it "should prevent a non-registered user from logging in" do
+        user = Marty::User.try_to_login('unknown_gemini', 'invalid_password')
+        expect(user).to be_nil
+        expect(Marty::User.current).to be_nil
+      end
+
+      it "should prevent a non-registered user from logging in when the " +
+        "database is in recovery mode" do
+        Marty::Util.stub(:db_in_recovery?).and_return(true)
+        user = Marty::User.try_to_login('unknown_gemini', 'invalid_password')
+        expect(user).to be_nil
+        expect(Marty::User.current).to be_nil
+      end
+    end
+  end
+end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -68,6 +68,7 @@ module Dummy
 
     # FIXME: not sure this is the proper way to add a new config
     config.marty = ActiveSupport::OrderedOptions.new
+    config.marty.local_password = 'gemini'
 
     config.secret_key_base = "SECRET_KEY_BASE"
   end
