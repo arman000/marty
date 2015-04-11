acaf96b25cec3f0a8ef760fd00bb700c41389937
Arman Bostani <abostani@pnmac.com>
2015-04-10 17:13:53 -0700

    Added creation of system login to seeds.  This is needed since we're creating Mcfly objects there.  Also, create BASE PostingType.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,31 @@
+# create system account if not there
+system_login = Rails.configuration.marty.system_account || 'marty'
+unless Marty::User.find_by_login(system_login)
+  user           = Marty::User.new
+  user.login     = system_login
+  user.firstname = system_login
+  user.lastname  = system_login
+  user.active    = true
+  user.save!
+end
+
+# FIXME: hacky -- globally changes whodunnit
+Mcfly.whodunnit = Marty::User.find_by_login(system_login)
+
 # Create all Marty roles from configuration
 (Rails.configuration.marty.roles || []).each do |role|
   Marty::Role.create(name: role.to_s)
 end
 
+# FIXME: this is a hack.  It's needed due to the fact that 'NOW'
+# posting requires a PostingType.
+Marty::PostingType.create name: 'BASE'
+
 # Create NOW posting
 unless Marty::Posting.find_by_name('NOW')
   sn                 = Marty::Posting.new
-  sn.posting_type_id = Marty::PostingType["BASE"].id
+  # FIXME: this is Gemini-specific
+  sn.posting_type_id = Marty::PostingType['BASE'].id
   sn.comment         = '---'
   sn.created_dt      = 'infinity'
   sn.save!
