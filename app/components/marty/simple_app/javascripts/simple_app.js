eca0d04831c98bdc4a5954e8bacdc9670d3262e7
Max Gorin <gorinme@gmail.com>
2015-06-16 10:06:01 +0300

    Fix routing

--- app/components/marty/simple_app/javascripts/simple_app.js
+++ app/components/marty/simple_app/javascripts/simple_app.js
@@ -6,20 +6,22 @@
     this.menuBar   = this.down('container[itemId="menu_bar"]');
     var statusBar = this.statusBar = this.down('container[itemId="status_bar"]');
 
-
     // Setting the "busy" indicator for Ajax requests
     Ext.Ajax.on('beforerequest',    function(){ statusBar.showBusy(); });
     Ext.Ajax.on('requestcomplete',  function(){ statusBar.hideBusy(); });
     Ext.Ajax.on('requestexception', function(){ statusBar.hideBusy(); });
 
-    // Initialize history
-    Ext.util.History.init();
-    Ext.util.History.on('change', this.processHistory, this);
+    this.setRouting();
   },
 
-  processHistory: function(token){
+  setRouting: function(){
+    this.router = Ext.util.History;
+    this.router.init();
+    this.router.on('change', this.loadRoute, this);
+  },
+
+  loadRoute: function(token){
     if (token){
-      this.mainPanel.removeAll();
       this.netzkeLoadComponent(token, {container: this.mainPanel});
     } else {
       this.mainPanel.removeAll();
@@ -28,28 +30,14 @@
 
   afterRender: function(){
     this.callParent();
-    var currentToken = Ext.util.History.getToken();
+    var currentToken = this.router.getToken();
     if (typeof currentToken == "string" && currentToken.length > 0) {
-      this.processHistory(currentToken);
+      this.loadRoute(currentToken);
     }
   },
 
-  appLoadComponent: function(name){
-    this.netzkeLoadComponent(name, {container: this.mainPanel});
-//    Ext.util.History.suspendEvent("change");
-    Ext.util.History.add(name);
-    // Can't re-enable history events right away because we don't
-    // want a second load of the same component (causes ExtJS to
-    // error). The back button won't function properly for the second
-    // this is disabled.
-//    setTimeout(function() {
-//      Ext.util.History.resumeEvent("change");
-//    }, 1000);
-  },
-
   netzkeLoadComponentByAction: function(action){
-    var componentName = action.component || action.name;
-    if (componentName) this.appLoadComponent(componentName);
+    this.router.add(action.name);
   },
 
   onToggleConfigMode: function(params){
