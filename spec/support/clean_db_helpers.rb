34addfedd86899ba095630ba8515db92e8d18226
Brian VanLoo <brian.vanloo@pnmac.com>
2015-07-10 09:48:05 -0700

    Fix problem with specs that load/save db images

--- spec/support/clean_db_helpers.rb
+++ spec/support/clean_db_helpers.rb
@@ -12,6 +12,7 @@ module CleanDbHelpers
     `pg_restore -j 2 -O -x -c -d #{current_db} #{clean_file}`
     `rm -f #{clean_file}`
     ActiveRecord::Base.clear_all_connections!
+    ActiveRecord::Base.shared_connection = nil
     self.use_transactional_fixtures = true
   end
 end
