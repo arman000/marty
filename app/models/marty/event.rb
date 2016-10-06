5142765da3b17591fa3e950cf76d91f385a5fb49
Jock Cooper <jock.cooper@pnmac.com>
2016-10-06 14:13:29 -0700

    additional methods for event

--- app/models/marty/event.rb
+++ app/models/marty/event.rb
@@ -20,20 +20,49 @@ class Marty::Event < Marty::Base
     end.present?
   end
 
+  def self.lookup_event(klass, subject_id, operation)
+    hash = all_running.select do |pm|
+      pm["klass"] == klass && pm["subject_id"] == subject_id.to_i &&
+        pm["enum_event_operation"] == operation
+    end.first
+    Marty::Event.find_by_id(hash["id"])
+  end
+
+  def self.finish_event(klass, subject_id, operation)
+    hash = all_running.select do |pm|
+      pm["klass"] == klass && pm["subject_id"] == subject_id.to_i &&
+        pm["enum_event_operation"] == operation
+    end.first
+    e = Marty::Event.find_by_id(hash["id"])
+    raise "can't explicitly finish a promise event" if e.promise_id
+    e.end_dt = Time.zone.now
+    e.save!
+  end
+
+  def self.last_event(klass, subject_id, operation=nil)
+    hash = all_running.select do |pm|
+      pm["klass"] == klass && pm["subject_id"] == subject_id.to_i &&
+        (operation.nil? || pm["enum_event_operation"] == operation)
+    end.sort { |a, b| b["start_dt"]  <=> a["start_dt"] }.first
+    Marty::Event.find_by_id(hash["id"])
+  end
+
   def self.currently_running(klass, subject_id)
     all_running.select do |pm|
-      pm["klass"] == klass && pm["subject_id"].to_i == subject_id.to_i
+      pm["klass"] == klass && pm["subject_id"] == subject_id.to_i
     end.map { |e| e["enum_event_operation"] }
   end
 
   def self.all_running
     @all_running ||= { timestamp: 0, data: [] }
+    @poll_secs ||= Marty::Config['MARTY_EVENT_POLL_SECS'] || 5
     time_now = Time.now.to_i
-    if time_now - @all_running[:timestamp] > 5
+    if time_now - @all_running[:timestamp] > @poll_secs
       @all_running[:data] =
         ActiveRecord::Base.connection.execute(
             "SELECT * FROM
-               (SELECT ev.klass,
+               (SELECT ev.id,
+                   ev.klass,
                    ev.subject_id,
                    ev.enum_event_operation,
                    coalesce(pr.start_dt, ev.start_dt) start_dt,
@@ -46,7 +75,14 @@ class Marty::Event < Marty::Base
              WHERE end_dt IS NULL
                AND (expire_secs IS NULL
                  OR expire_secs > EXTRACT (EPOCH FROM NOW() - start_dt));"
-      ).to_a
+      ).to_a.map do |h|
+        h["id"] = h["id"].to_i
+        h["subject_id"] = h["subject_id"].to_i
+        h["start_dt"] = Time.zone.parse(h["start_dt"])
+        h["end_dt"] = Time.zone.parse(h["end_dt"]) if h["end_dt"]
+        h["expire_secs"] = h["expire_secs"].to_i if h["expire_secs"]
+        h
+      end
       @all_running[:timestamp] = time_now
     end
     @all_running[:data]
