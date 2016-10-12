a2f14fbc5795619a8b9c5fb0d0572ce20f526156
mmatsuo <mmatsuo@gmail.com>
2016-10-12 10:58:35 -0700

    added all_events capability

--- app/models/marty/event.rb
+++ app/models/marty/event.rb
@@ -21,7 +21,7 @@ class Marty::Event < Marty::Base
   end
 
   def self.lookup_event(klass, subject_id, operation)
-    hash = all_running.select do |pm|
+    hash = all_events.select do |pm|
       pm["klass"] == klass && pm["subject_id"] == subject_id.to_i &&
         pm["enum_event_operation"] == operation
     end.first
@@ -40,7 +40,7 @@ class Marty::Event < Marty::Base
   end
 
   def self.last_event(klass, subject_id, operation=nil)
-    hash = all_running.select do |pm|
+    hash = all_events.select do |pm|
       pm["klass"] == klass && pm["subject_id"] == subject_id.to_i &&
         (operation.nil? || pm["enum_event_operation"] == operation)
     end.sort { |a, b| b["start_dt"]  <=> a["start_dt"] }.first
@@ -51,6 +51,18 @@ class Marty::Event < Marty::Base
     all_running.select do |pm|
       pm["klass"] == klass && pm["subject_id"] == subject_id.to_i
     end.map { |e| e["enum_event_operation"] }
+  end
+
+  def to_display
+    d = self.enum_event_operation.downcase.capitalize
+    if self.end_dt
+      return d.sub(/ing/, 'ed')
+    end
+    d
+  end
+
+  def compact_end_dt
+    self.end_dt ? self.end_dt.strftime("%H:%M") : '---'
   end
 
   def self.all_running
@@ -88,4 +100,35 @@ class Marty::Event < Marty::Base
     @all_running[:data]
   end
   private_class_method :all_running
+
+  def self.all_events
+     @all_running ||= { timestamp: 0, data: [] }
+    @poll_secs ||= Marty::Config['MARTY_EVENT_POLL_SECS'] || 5
+    time_now = Time.now.to_i
+    if time_now - @all_running[:timestamp] > @poll_secs
+      @all_running[:data] =
+        ActiveRecord::Base.connection.execute(
+               "SELECT ev.id,
+                   ev.klass,
+                   ev.subject_id,
+                   ev.enum_event_operation,
+                   coalesce(pr.start_dt, ev.start_dt) start_dt,
+                   coalesce(pr.end_dt, ev.end_dt) end_dt,
+                   expire_secs
+                FROM marty_events ev
+                LEFT JOIN marty_promises pr ON ev.promise_id = pr.id;"
+      ).to_a.map do |h|
+        h["id"] = h["id"].to_i
+        h["subject_id"] = h["subject_id"].to_i
+        h["start_dt"] = Time.zone.parse(h["start_dt"])
+        h["end_dt"] = Time.zone.parse(h["end_dt"]) if h["end_dt"]
+        h["expire_secs"] = h["expire_secs"].to_i if h["expire_secs"]
+        h["comment"] = h["comment"]
+        h
+      end
+      @all_running[:timestamp] = time_now
+    end
+    @all_running[:data]
+  end
+  private_class_method :all_events
 end
