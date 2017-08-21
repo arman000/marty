1b46ddb5f32c954a366d04518b7e31ab54127e73
eugene <ezvy@yandex.ru>
2017-08-21 22:45:24 +0700

    Get rid of redundant :action in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 end
 
 Marty::Engine.routes.draw do
-  match via: [:get, :post], "rpc/:action(.:format)" => "rpc", as: :rpc
-  get "job/:action" => "job", as: :job
+  match via: [:get, :post], "rpc/evaluate(.:format)" => "rpc", as: :rpc
+  get "job/download" => "job", as: :job
   match via: [:get, :post], "report(.:format)" => "report#index", as: :report
   get  'diag', to: 'diagnostic/#op'
 end
