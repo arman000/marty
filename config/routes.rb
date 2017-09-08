849c11432ba28fa940bec73b6a13ede23b9df7f1
eugene <ezvy@yandex.ru>
2017-09-08 20:57:14 +0700

    Remove obsolete firefox driver, cleanup components

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Marty::Engine.routes.draw do
   match via: [:get, :post], "rpc/evaluate(.:format)" => "rpc", as: :rpc
-  get "job/download" => "job", as: :job
   match via: [:get, :post], "report(.:format)" => "report#index", as: :report
+  get "job/download" => "job", as: :job
 
   %i(index version environment).each do |action|
-    get  "diag/#{action}", controller: 'diagnostic'
+    get "diag/#{action}", controller: 'diagnostic'
   end
 
   %i(index version environment).each do |action|
-    get  "diagnostic/#{action}", controller: 'diagnostic'
+    get "diagnostic/#{action}", controller: 'diagnostic'
   end
 end
