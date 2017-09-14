1a930abbdc58099660cf2bac9259d7cff46cc768
Arman Bostani <arman.bostani@pnmac.com>
2017-09-14 08:44:44 -0700

    code reformat

--- app/components/marty/config_view.rb
+++ app/components/marty/config_view.rb
@@ -1,25 +1,25 @@
 class Marty::ConfigView < Marty::Grid
-  has_marty_permissions \
-  create: :admin,
-  read: :admin,
-  update: :admin,
-  delete: :admin
+  has_marty_permissions create: :admin,
+                        read: :admin,
+                        update: :admin,
+                        delete: :admin
 
   include Marty::Extras::Layout
 
   def configure(c)
     super
 
-    c.title   = I18n.t('config', default: "Config")
-    c.model   = "Marty::Config"
+    c.title      = I18n.t('config', default: "Config")
+    c.model      = "Marty::Config"
     c.attributes = [:key, :value, :description]
-    c.store_config.merge!({sorters: [{property: :key, direction: 'ASC'}]})
-    c.editing = :both
+    c.editing    = :both
+    c.store_config.merge!(sorters: [{property: :key, direction: 'ASC'}])
   end
 
   def my_jsonb_getter
     lambda { |r| v = Marty::Config[r.key]; v && v.to_json || '' }
   end
+
   def my_jsonb_pretty_getter
     lambda { |r| v = Marty::Config[r.key]
       v && (JSON.pretty_generate(v) rescue v.to_json) || '' }
@@ -37,8 +37,8 @@ class Marty::ConfigView < Marty::Grid
     [
       :key,
       jsonb_field(:value, {
-                    getter:  my_jsonb_pretty_getter,
-                    setter:  my_jsonb_setter,
+                    getter: my_jsonb_pretty_getter,
+                    setter: my_jsonb_setter,
                   }),
       textarea_field(:description),
     ]
@@ -49,14 +49,14 @@ class Marty::ConfigView < Marty::Grid
   end
 
   column :value do |c|
-    c.flex = 3
-    c.getter  = my_jsonb_getter
-    c.setter  = my_jsonb_setter
+    c.flex   = 3
+    c.getter = my_jsonb_getter
+    c.setter = my_jsonb_setter
   end
+
   attribute :description do |c|
     c.flex = 1
   end
-
 end
 
 ConfigView = Marty::ConfigView
