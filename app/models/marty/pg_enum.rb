af2fbf0eea72b6809013155c2fea4e0a13f5699d
Arman Bostani <arman.bostani@pnmac.com>
2018-01-08 14:44:01 -0800

    Update enum delorean_fn to take optional (ignored) pt argument for compatibility of Gemini's loan_program & security_instrument.

--- app/models/marty/pg_enum.rb
+++ app/models/marty/pg_enum.rb
@@ -1,7 +1,8 @@
 module Marty::PgEnum
 
-  def [](index)
-    index = index.to_s
+  def [](i0, i1=nil)
+    # if i1 is provided, then i0 is a pt and we ignore it.
+    index = (i1 || i0).to_s
 
     raise "no such #{self.name}: '#{index}'" unless
       self::VALUES.include?(index)
@@ -9,18 +10,15 @@ module Marty::PgEnum
     StringEnum.new(index)
   end
 
-  def get_all
+  def get_all(pt=nil)
     self::VALUES.map { |v| StringEnum.new(v) }
   end
 
-  GET_ALL_SIG = [0, 0]
-  LOOKUP_SIG = [1, 1]
-  FIND_BY_NAME_SIG = [1, 1]
   def self.extended(base)
     base.class_eval do
-      const_set :GET_ALL_SIG, Marty::PgEnum::GET_ALL_SIG
-      const_set :LOOKUP_SIG, Marty::PgEnum::LOOKUP_SIG
-      const_set :FIND_BY_NAME_SIG, Marty::PgEnum::FIND_BY_NAME_SIG
+      const_set :GET_ALL_SIG,      [0, 1]
+      const_set :LOOKUP_SIG,       [1, 2]
+      const_set :FIND_BY_NAME_SIG, [1, 2]
     end
   end
 
