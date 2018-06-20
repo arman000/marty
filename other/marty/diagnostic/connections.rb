5a506415b1c8953b70f467dcdb663ec3030742f6
Hayden McFarland <hayden.mcfarland@pnmac.com>
2018-06-20 10:13:02 -0700

    sort by state for connections diag

--- other/marty/diagnostic/connections.rb
+++ other/marty/diagnostic/connections.rb
@@ -2,7 +2,10 @@ module Marty::Diagnostic; class Connections < Base
   self.aggregatable = false
   diagnostic_fn do
     conns = Node.get_postgres_connections[Database.db_name].
-              sort_by{|h| [h['application_name'], h['pid'], h['client_addr']]}
+              sort_by{|h| [h['application_name'],
+                           h['pid'],
+                           h['client_addr'],
+                           h['state']]}
 
     counts = Hash.new(0)
     conns.each_with_object({}) do |c, h|
