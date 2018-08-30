d83324872df4a30bd0420b70ed03b0482b8ea462
Frank Garcia <frank.a.garcia@pnmac.com>
2018-08-30 12:41:30 -0700

    moved majority of genreate_exclusions logic out of Marty::SqlRule

--- app/models/marty/sql_rule.rb
+++ app/models/marty/sql_rule.rb
@@ -1,22 +1,7 @@
 class Marty::SqlRule < Marty::BaseRule
   self.abstract_class = true
 
-  def self.generate_exclusions(attr_mdl, rule_type)
-    exclusions = []
-
-    self.where(rule_type: rule_type).each do |rule|
-      executions_affected = []
-      rule_attrs = attr_mdl.where(rule_id: rule.id)
-      rule_attrs.each do |attr|
-        executions_affected.append(self.generate_executions_affected(attr))
-      end
-
-      executions_empty = executions_affected.all? {|x| x == []}
-      if !executions_empty
-        exclusions.append({rule.id => executions_affected.flatten})
-      end
-    end
-
+  def self.generate_exclusions(exclusions)
     Marty::Exclusion.new(exclusions)
   end
 
