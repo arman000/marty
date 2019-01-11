c1da29dc6632c9ae3196287e53fbed8f810e2a85
eugene <ezvy@yandex.ru>
2019-01-11 16:53:32 +0300

    Fix the failing delayed job spec. It was failing due to changes in models/marty/promise. Before, Promise would run delayed job in the same thread if it wasn't runing already. Not this behavior removed. Before that spec was passing only because the promise was executing it's delayed job right away. Delayed job process that was running in the background couldn't help. Now, since delayed job can only be executed in the background, nothing happens, because transactional tests make AR never commit it's transactions, only add the savepoints. So other threads can not access this data, because it's never commited.

--- spec/other/diagnostic/delayed_job_version_spec.rb
+++ spec/other/diagnostic/delayed_job_version_spec.rb
@@ -11,9 +11,17 @@ describe Marty::Diagnostic::DelayedJobVersion do
   end
 
   before(:each) do
+    Delayed::Worker.delay_jobs = false
     Marty::Script.load_scripts(nil, Date.today)
     allow(described_class).to receive(:scope).and_return(nil)
+    start_delayed_job
   end
+
+  after(:each) do
+    Delayed::Worker.delay_jobs = true
+    stop_delayed_job
+  end
+
 
   def sample_data
     {
@@ -29,15 +37,11 @@ describe Marty::Diagnostic::DelayedJobVersion do
 
   it 'can detect if all workers are running correct application version' do
     ENV['DELAYED_VER'] = Marty::VERSION
-    start_delayed_job
     expect(described_class.generate).to eq(sample_data)
-    stop_delayed_job
   end
 
   it 'will fail if DELAYED_VER is not set' do
     ENV.delete('DELAYED_VER')
-    start_delayed_job
     expect{described_class.generate}.to raise_error(RuntimeError)
-    stop_delayed_job
   end
 end
--- spec/support/shared_connection_db_helpers.rb
+++ spec/support/shared_connection_db_helpers.rb
@@ -4,15 +4,55 @@ module Marty; module RSpec; module SharedConnectionDbHelpers
   end
 
   def save_clean_db(clean_file)
-    `pg_dump -O -Fc #{current_db} > #{clean_file}`
+    if db_host == 'localhost'
+      `pg_dump -O -Fc #{current_db} > #{clean_file}`
+    else
+      `#{remote_db_pw} pg_dump -O -Fc #{remote_db_args} #{current_db} > #{clean_file}`
+    end
   end
 
   def restore_clean_db(clean_file, remove_file = true)
     self.use_transactional_tests = false
-    `pg_restore -j 2 -O -x -c -d #{current_db} #{clean_file}`
+
+    if db_host == 'localhost'
+      `pg_restore -j 2 -O -x -c -d #{current_db} #{clean_file}`
+    else
+      `#{remote_db_pw} pg_restore #{remote_db_args} #{restore_args(current_db, clean_file)}`
+    end
+
     `rm -f #{clean_file}` if remove_file
     ActiveRecord::Base.clear_all_connections!
     ActiveRecord::Base.reset_shared_connection
     self.use_transactional_tests = true
+  end
+
+  private
+
+  def current_db
+    ActiveRecord::Base.connection_config[:database]
+  end
+
+  def db_host
+    ActiveRecord::Base.connection_config[:host]
+  end
+
+  def db_user
+    ActiveRecord::Base.connection_config[:username]
+  end
+
+  def db_password
+    ActiveRecord::Base.connection_config[:password]
+  end
+
+  def restore_args db, from
+    "-j 2 -O -x -c -d #{db} #{from}"
+  end
+
+  def remote_db_args
+    "-h #{db_host} -U #{db_user} -w"
+  end
+
+  def remote_db_pw
+    "PGPASSWORD='#{db_password}'"
   end
 end end end
