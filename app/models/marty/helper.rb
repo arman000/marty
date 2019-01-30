e6d7d72844c9201110d84a05b4f92d749220c010
Eugene Zvyagintsev <eugene.zvyagintsev@pnmac.com>
2019-01-29 17:06:20 -0800

    Add blame and table report to Marty

--- app/models/marty/helper.rb
+++ app/models/marty/helper.rb
@@ -18,4 +18,37 @@ class Marty::Helper
   delorean_fn :git, sig:0 do
     [my_ip, ENV['DELAYED_VER']]
   end
+
+  delorean_fn :infinity_dt, sig: 1 do |pt|
+    Mcfly.is_infinity pt
+  end
+
+  delorean_fn :constantize, sig: 1 do |class_name|
+    raise 'bad class_name' unless class_name.is_a?(String)
+    class_name.constantize
+  end
+
+  delorean_fn :get_column_types, sig: 1 do |klass|
+    Marty::DataConversion.col_types(klass)
+  end
+
+  delorean_fn :parse_csv_to_hash, sig: 3 do |txt, comma_sep, types|
+    txt ||= ""
+    headers, *rows = CSV.parse(txt.strip,
+                               headers: true,
+                               col_sep: (comma_sep ? "," : "\t")).to_a
+    rows.map do |row|
+      headers.zip(row).each_with_object({}) do |(h, v), res|
+        res[h] = v.blank? ? nil :
+                   Marty::DataConversion.convert(v, (types[h] || "text").to_sym)
+      end
+    end
+  end
+
+  delorean_fn :to_csv, sig: [1, 2] do |*args|
+    # NOTE: can't use |data, config| due to delorean_fn weirdness.
+    data, config = args
+
+    Marty::DataExporter.to_csv(data, config)
+  end
 end
