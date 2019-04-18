dbfd3d7d805dcec34dbe69c8b20ccc57517f6d39
Eugene Zvyagintsev <eugene.zvyagintsev@pnmac.com>
2019-04-18 08:57:41 -0700

    Allow to controll access to the endpoints via has_marty_permissions. Before that any logged in user could access the endpoint.
    If has_marty_permissions doesn't specify permissions for endpoint, this
    endpoint will be accessible as before for backward compatibility.

---
+++ spec/features/endpoint_access.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature 'Endpoint access', js: true do
+  before do
+    populate_test_users
+  end
+
+  context 'as admin' do
+    before do
+      log_in_as('admin1')
+      wait_for_ajax
+    end
+
+    it 'can access test_access endpoint' do
+      press('Pricing Config.')
+      press('Loan Programs')
+      press('Test Access')
+      wait_for_ajax
+      expect(page).to have_content 'You have admin access'
+    end
+  end
+
+  context 'as dev' do
+    before do
+      log_in_as('dev1')
+      wait_for_ajax
+    end
+
+    it 'can not access test_access endpoint' do
+      press('Pricing Config.')
+      press('Loan Programs')
+      press('Test Access')
+      wait_for_ajax
+      expect(page).to have_content 'Permission Denied'
+    end
+  end
+end
