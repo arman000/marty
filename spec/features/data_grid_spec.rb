10274e0f06763cff9d5090edf6f71bf9782aa5ec
Jock Cooper <jock.cooper@pnmac.com>
2019-05-21 17:29:05 -0700

    WIP

--- spec/features/data_grid_spec.rb
+++ spec/features/data_grid_spec.rb
@@ -11,11 +11,7 @@ feature 'data grid view', js: true do
     p = File.expand_path('../../fixtures/misc', __FILE__)
     Dir.glob(p + '/data_grid_*.txt').each do |path|
       n = File.basename(path, '.txt').camelize
-      begin
-        Marty::DataGrid.create_from_import(n, File.read(path), dt)
-      rescue => e
-        binding.pry
-      end
+      Marty::DataGrid.create_from_import(n, File.read(path), dt)
     end
   end
   after(:all) do
@@ -27,18 +23,18 @@ feature 'data grid view', js: true do
     expect(page).to have_content 'Data Grids'
   end
 
-  def grid_setup
+  def grid_setup(rows)
     @grid = page.all(:xpath,
                     ".//div[contains(@class, 'x-grid-item-container')]").
-             detect{ |e| e.text.include?('flavor / param') }
-
+             reject{ |e| e.text.include?('2016-12-31') }.first
     @gridx = @grid.native.rect.x
+    @gridy = @grid.native.rect.y
     height = @grid.native.size.height
-    @perr = (height / 3).to_i
+    @perr = (height / rows).to_i
     cols=page.all(:xpath, ".//div[contains(@role, 'columnheader') and "\
                           "contains(@class, 'x-column-header')]").select do
       |c|
-      (0..10).map(&:to_s).include?(c.text)
+      (0..100).map(&:to_s).include?(c.text)
     end.map { |c| n=c.native; [n.text.to_i, [n.rect.x, n.rect.width]] }
     @colh = Hash[cols]
   end
@@ -51,8 +47,15 @@ feature 'data grid view', js: true do
     mypos = ypos + (@perr*menuidx+ @perr/2).to_i
     page.driver.browser.action.
       move_to(@grid.native, xpos, ypos).context_click.perform
-    page.driver.browser.action.move_to(@grid.native, mxpos, mypos).click.
-      release.perform if click
+
+    if click
+      page.driver.browser.action.move_to(@grid.native, mxpos, mypos).click.
+        release.perform if click
+    else
+      ret = get_menu
+      page.driver.browser.action.move_to(@grid.native, 0,0).click.perform
+      ret
+    end
   end
   def get_menu
       m=page.all(:xpath, ".//div[contains(@class, 'x-menu-item-default')]").to_a
@@ -110,17 +113,13 @@ feature 'data grid view', js: true do
     #
   end
   def check_grid(grid_name)
-    get_latest = lambda do
-      Marty::DataGrid.mcfly_pt('infinity').find_by(name: grid_name)
-    end
-    grid = get_latest.call()
+    grid = Marty::DataGrid.mcfly_pt('infinity').find_by(name: grid_name)
     hdim = grid.metadata.select { |md| md['dir'] == 'h' }
     vdim = grid.metadata.select { |md| md['dir'] == 'v' }
     data = grid.data
     ui_data = get_grid
     row_cnt = ui_data.length
     col_cnt = ui_data[0].length
-    # colors check
     ui_colors = get_grid(false)
 
     vdim_size = vdim.count == 0 ? 1 : vdim.count
@@ -143,46 +142,38 @@ feature 'data grid view', js: true do
       end
       sub
     end
-    # vertical dims
-    if vdim.count > 0
-      vert_color_sets = vdim_size.times.map { Set.new }
-      iterate_area.call(vdim_area, ui_colors) do |cell, row_idx, col_idx|
-        vert_color_sets[col_idx] << cell
-      end
-      vert_color_sets.each do |theset|
-        expect(theset.length).to eq(1)
-        expect(theset.to_a[0]).to match(/^rgb/)
-      end
-      all_set = vert_color_sets.map(&:to_a).reduce(&:+).to_set
-      expect(all_set.length).to eq(vert_color_sets.length)
 
-      vdim_data = iterate_area.call(vdim_area, ui_data)
-      keys = vdim.map { |d| Marty::DataGrid.export_keys(d) }
-
-      piv = keys.each_with_object([]) do |key_array, piv_array|
-        key_array.each_with_index do |v, idx|
-          (piv_array[idx] ||= []) << v
+    # colors / data check in dim area
+    check_dim = lambda do |dim, dim_size, dim_area, by_col: true|
+      if dim.count > 0
+        color_sets = Array.new(dim_size) { Set.new }
+        iterate_area.call(dim_area, ui_colors) do |cell, row_idx, col_idx|
+          idx = by_col ? col_idx : row_idx
+          color_sets[idx] << cell
         end
-      end
-      expect(piv.map(&:flatten)).to eq(vdim_data)
-    end
-    # horizontal dims
-    if hdim.count > 0
-      horiz_color_sets = hdim_size.times.map { Set.new }
-      iterate_area.call(hdim_area, ui_colors) do |cell, row_idx, col_idx|
-        horiz_color_sets[row_idx] << cell
-      end
-      horiz_color_sets.each do |theset|
-        expect(theset.length).to eq(1)
-        expect(theset.to_a[0]).to match(/^rgb/)
-      end
-      all_set = horiz_color_sets.map(&:to_a).reduce(&:+).to_set
-      expect(all_set.length).to eq(horiz_color_sets.length)
+        color_sets.each do |the_set|
+          expect(the_set.length).to eq(1)
+          expect(the_set.to_a[0]).to match(/^rgb/)
+        end
+        all_set = color_sets.map(&:to_a).reduce(&:+).to_set
+        expect(all_set.length).to eq(color_sets.length)
 
-      hdim_data = iterate_area.call(hdim_area, ui_data)
+        dim_data = iterate_area.call(dim_area, ui_data)
+        keys = dim.map { |d| Marty::DataGrid.export_keys(d) }
+        piv = by_col ?
+                keys.each_with_object([]) do |key_array, piv_array|
+                  key_array.each_with_index do |v, idx|
+                    (piv_array[idx] ||= []) << v
+                  end
+                end : keys
+        expect(piv.map(&:flatten)).to eq(dim_data)
+      end
     end
+      # vertical dims
+    check_dim.call(vdim, vdim_size, vdim_area)
+    check_dim.call(hdim, hdim_size, hdim_area, by_col: false)
 
-    # data and label section
+    # data and label section color check
     [data_area, label_area].each do |area|
       colors = Set.new
       iterate_area.call(area, ui_colors) do |cell, row_idx, col_idx|
@@ -190,8 +181,66 @@ feature 'data grid view', js: true do
       end
       expect(colors.length).to eq(1)
       expect(colors.to_a[0]).to eq('')
-      data_data = iterate_area.call(data_area, ui_data)
-      label_data = iterate_area.call(label_area, ui_data)
+    end
+
+    # data and label section data check
+    data_data = iterate_area.call(data_area, ui_data)
+    expect(struct_compare(data_data, grid.data)).to be_falsey
+    label_data = iterate_area.call(label_area, ui_data)
+    label_exp = Array.new(hdim_size) { Array.new(vdim_size) }
+    hdim_labels = hdim.map { |d| d['attr'] }
+    vdim_labels = vdim.map { |d| d['attr'] }
+    hdim_labels.each_with_index do |label, idx|
+      label_exp[idx][-1] = label
+    end
+    vdim_labels.each_with_index do |label, idx|
+      cur = label_exp.last[idx]
+      label_exp.last[idx] = cur.is_a?(String) ? label + ' / ' + cur : label
+    end
+    expect(struct_compare(label_data, label_exp)).to be_falsey
+
+    # check context menu
+    grid_setup(hdim_size + grid.data.length)
+    all_disabled = {
+      "Insert Row Above"=>:disabled,
+      "Insert Row Below"=>:disabled,
+      "Insert Column Left"=>:disabled,
+      "Insert Column Right"=>:disabled,
+      "Delete Row"=>:disabled,
+      "Delete Column"=>:disabled
+    }
+    col_disabled = all_disabled + {
+      "Insert Row Above"=>:enabled,
+      "Insert Row Below"=>:enabled,
+      "Delete Row"=>:enabled
+    }
+    row_disabled = all_disabled + {
+      "Insert Column Left"=>:enabled,
+      "Insert Column Right"=>:enabled,
+      "Delete Column"=>:enabled
+    }
+    all_enabled = Hash[all_disabled.map { |k, v| [k, :enabled] }]
+
+    data_menu = case
+                when hdim.count == 0
+                  col_disabled
+                when vdim.count == 0
+                  row_disabled
+                else
+                  all_enabled
+                end
+    areas = [[data_area, data_menu],
+             [label_area, all_disabled],
+             [vdim_area, vdim.count > 0 ? col_disabled : all_disabled],
+             [hdim_area, hdim.count > 0 ? row_disabled : all_disabled]]
+    areas.each do |area, menu_exp|
+      (area[:ulx]...area[:lrx]).each do |x|
+        (area[:uly]...area[:lry]).each do |y|
+          m = context_click(x, y, 0)
+          binding.pry if m != menu_exp
+          expect(m).to eq(menu_exp)
+        end
+      end
     end
 
   end
@@ -202,13 +251,88 @@ feature 'data grid view', js: true do
     dgv = netzke_find('data_grid_view')
     grids = dgv.get_col_vals('name', 5)
     grids.each do |grid|
-
       pos = grids.index(grid) + 1
       dgv.select_row(pos)
       press('Edit Grid')
+      wait_for_ajax
       check_grid(grid)
       press('Cancel')
       wait_for_ajax
+    end if false
+
+    get_latest = lambda do
+      Marty::DataGrid.mcfly_pt('infinity').find_by(name: 'DataGrid5')
     end
+    pos = grids.index('DataGrid5') + 1
+    dgv.select_row(pos)
+    press('Edit Grid')
+    wait_for_ajax
+    # test saving and validation etc
+    grid_setup(12) # 12 rows of data
+    cell_edit(2,3,'abc')
+    press('Save')
+    expect(page).to have_content('error: bad float "abc"')
+    press('OK')
+    cell_edit(2,3,'123.456')
+    press('Save')
+    wait_for_ajax
+    grid = get_latest.call
+    expect(grid.data[0][0]).to eq(123.456)
+    press('Edit Grid')
+    wait_for_ajax
+    grid_setup(12) # 12 rows of data
+    cell_edit(2,3,'0.0')
+    press('Cancel')
+    exp = 'You are closing a window that has unsaved changes'
+    expect(page).to have_content(exp)
+    press('No')
+    find(:xpath, ".//div[contains(@class,'x-tool-close')]").click
+    exp = 'You are closing a window that has unsaved changes'
+    expect(page).to have_content(exp)
+    press('No')
+    press('Cancel')
+    exp = 'You are closing a window that has unsaved changes'
+    expect(page).to have_content(exp)
+    press('Yes')
+    grid = get_latest.call
+    expect(grid.data[0][0]).to eq(123.456)
+
+    press('Edit Grid')
+    wait_for_ajax
+    grid_setup(12) # 12 rows of data
+    context_click(1,9,1, click: true)
+    grid_setup(13) # 12 rows of data
+    ['AR', 'Abc', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].each_with_index do |v, idx|
+      cell_edit(idx, 10, v)
+    end
+    context_click(5,12, 0, click: true)
+    grid_setup(14) # 12 rows of data
+    ['TN', 'Xyz', 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1].each_with_index do |v, idx|
+      cell_edit(idx, 12, v)
+    end
+    context_click(5, 5, 2, click: true)
+    grid_setup(14) # 12 rows of data
+    (['FHA', nil, '<1'] + [100]*11).each_with_index do |v, idx|
+      cell_edit(5, idx, v) if v
+    end
+    context_click(13, 1, 3, click: true)
+    grid_setup(14) # 12 rows of data
+    (['VA', nil, '>5'] + [100]*11).each_with_index do |v, idx|
+      cell_edit(14, idx, v)
+    end
+    grid_setup(14) # 12 rows of data
+    context_click(2, 4, 4, click: true)
+    grid_setup(13) # 12 rows of data
+    context_click(2, 1, 5, click: true)
+    wait_for_ajax
+    # FIXME weird behavior here.  save doesn't close window
+    press('Save')
+    wait_for_ajax
+    grid = get_latest.call
+    exp_data = JSON.parse(File.read('spec/fixtures/misc/grid_final_data.json'))
+    expect(grid.data).to eq (exp_data)
+    exp_meta = JSON.parse(File.read('spec/fixtures/misc/grid_final_meta.json'))
+    grid_meta = grid.metadata.map {|md| [md['dir'], md['attr'], md['keys']]}.sort
+    expect(grid_meta).to eq(exp_meta)
   end
 end
