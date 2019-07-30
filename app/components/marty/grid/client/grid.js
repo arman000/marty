de4c95f0dde4ab0b5d45369de996829e744edaa4
Hayden McFarland <hayden.mcfarland@pnmac.com>
2019-07-30 12:18:35 -0700

    add auto_size_column functionality

--- app/components/marty/grid/client/grid.js
+++ app/components/marty/grid/client/grid.js
@@ -1,162 +1,175 @@
 {
-  getComponent: function(name) {
-    return Ext.getCmp(name);
-  },
+    getComponent: function(name) {
+	return Ext.getCmp(name);
+    },
 
-  findComponent: function(name) {
-    return Ext.ComponentQuery.query(`[name=${name}]`)[0];
-  },
+    findComponent: function(name) {
+	return Ext.ComponentQuery.query(`[name=${name}]`)[0];
+    },
 
-  setDisableComponentActions: function(prefix, flag) {
-    for (var key in this.actions) {
-      if (key.substring(0, prefix.length) == prefix) {
-        this.actions[key].setDisabled(flag);
-      }
-    }
-  },
+    setDisableComponentActions: function(prefix, flag) {
+	for (var key in this.actions) {
+	    if (key.substring(0, prefix.length) == prefix) {
+		this.actions[key].setDisabled(flag);
+	    }
+	}
+    },
 
-  initComponent: function() {
-    this.dockedItems = this.dockedItems || [];
-    if (this.paging == 'pagination') {
-      this.dockedItems.push({
-        xtype: 'pagingtoolbar',
-        dock: 'bottom',
-        layout: {
-          overflowHandler: 'Menu'
-        },
-        listeners: {
-          'beforechange': this.disableDirtyPageWarning ? {} : {
-            fn: this.netzkeBeforePageChange,
-            scope: this
-          }
-        },
-        store: this.store,
-        items: this.bbar && ["-"].concat(this.bbar)
-      });
-    } else if (this.bbar) {
-      this.dockedItems.push({
-        xtype: 'toolbar',
-        dock: 'bottom',
-        layout: {
-          overflowHandler: 'Menu'
-        },
-        items: this.bbar
-      });
-    }
+    initComponent: function() {
+	// add auto size column functionality if enabled on attribute
+	this.viewConfig = {
+	    listeners: {
+		refresh: (view) => {
+		    Ext.each(view.panel.columns, (col) => {
+			if (col.autoSizeColumn === true) {
+			    col.autoSize();
+			}
+		    })
+		}
+	    }
+	}
 
-    // block creation of toolbars in parent
-    delete this.bbar;
-    var paging = this.paging
-    if (paging != 'buffered') {
-      this.paging = false;
-    }
-    this.callParent();
-    this.paging = paging
+	this.dockedItems = this.dockedItems || [];
+	if (this.paging == 'pagination') {
+	    this.dockedItems.push({
+		xtype: 'pagingtoolbar',
+		dock: 'bottom',
+		layout: {
+		    overflowHandler: 'Menu'
+		},
+		listeners: {
+		    'beforechange': this.disableDirtyPageWarning ? {} : {
+			fn: this.netzkeBeforePageChange,
+			scope: this
+		    }
+		},
+		store: this.store,
+		items: this.bbar && ["-"].concat(this.bbar)
+	    });
+	} else if (this.bbar) {
+	    this.dockedItems.push({
+		xtype: 'toolbar',
+		dock: 'bottom',
+		layout: {
+		    overflowHandler: 'Menu'
+		},
+		items: this.bbar
+	    });
+	}
 
-    var me = this;
+	// block creation of toolbars in parent
+	delete this.bbar;
+	var paging = this.paging
+	if (paging != 'buffered') {
+	    this.paging = false;
+	}
+	this.callParent();
+	this.paging = paging
 
-    var children = me.serverConfig.child_components || [];
-    me.onSelectionChange(
-      function(m) {
-        var has_sel = m.hasSelection();
+	var me = this;
 
-        var rid = null;
-        if (has_sel) {
-          if (m.type == 'spreadsheet') {
-            var cell = m.getSelected().startCell;
-            rid = cell && cell.record.getId();
-          }
-          if (!rid) {
-            selected = m.getSelection()[0];
-            rid = selected && selected.getId();
-          }
-        }
+	var children = me.serverConfig.child_components || [];
+	me.onSelectionChange(
+	    function(m) {
+		var has_sel = m.hasSelection();
 
-        me.serverConfig.selected = rid;
-        me.setDisableComponentActions('do', !has_sel);
+		var rid = null;
+		if (has_sel) {
+		    if (m.type == 'spreadsheet') {
+			var cell = m.getSelected().startCell;
+			rid = cell && cell.record.getId();
+		    }
+		    if (!rid) {
+			selected = m.getSelection()[0];
+			rid = selected && selected.getId();
+		    }
+		}
 
-        for (var child of children) {
-          var comp = me.findComponent(child)
-          if (comp) {
-            comp.serverConfig.parent_id = rid;
-            if (comp.setDisableComponentActions) {
-              comp.setDisableComponentActions('parent', !has_sel);
-            }
-            if (comp.reload) {
-              comp.reload()
-            }
-          }
-        }
-      });
+		me.serverConfig.selected = rid;
+		me.setDisableComponentActions('do', !has_sel);
 
-    var store = me.getStore();
-    var linked = me.serverConfig.linked_components || [];
-    for (var event of ['update', 'netzkerefresh']) {
-      store.on(event, function() {
-        for (var link of linked) {
-          var comp = me.findComponent(link);
-          if (comp && comp.reload) {
-            comp.reload()
-          }
-        }
-      }, this);
-    }
-  },
+		for (var child of children) {
+		    var comp = me.findComponent(child)
+		    if (comp) {
+			comp.serverConfig.parent_id = rid;
+			if (comp.setDisableComponentActions) {
+			    comp.setDisableComponentActions('parent', !has_sel);
+			}
+			if (comp.reload) {
+			    comp.reload()
+			}
+		    }
+		}
+	    });
 
-  onSelectionChange: function(f) {
-    var me = this;
-    me.getSelectionModel().on('selectionchange', f);
-  },
+	var store = me.getStore();
+	var linked = me.serverConfig.linked_components || [];
+	for (var event of ['update', 'netzkerefresh']) {
+	    store.on(event, function() {
+		for (var link of linked) {
+		    var comp = me.findComponent(link);
+		    if (comp && comp.reload) {
+			comp.reload()
+		    }
+		}
+	    }, this);
+	}
+    },
 
-  // override netzkeReloadStore to allow option passthrough
-  // reference: http://api.netzke.org/client/files/doc_client_netzke-basepack_javascripts_grid_event_handlers.js.html
-  netzkeReloadStore: function(opts = {}) {
-    var store = this.getStore();
+    onSelectionChange: function(f) {
+	var me = this;
+	me.getSelectionModel().on('selectionchange', f);
+    },
 
-    // HACK to work around buffered store's buggy reload()
-    if (!store.lastRequestStart) {
-      store.load(opts);
-    } else store.reload(opts);
-  },
+    // override netzkeReloadStore to allow option passthrough
+    // reference: http://api.netzke.org/client/files/doc_client_netzke-basepack_javascripts_grid_event_handlers.js.html
+    netzkeReloadStore: function(opts = {}) {
+	var store = this.getStore();
 
-  doViewInForm: function(record) {
-    this.netzkeLoadComponent("view_window", {
-      serverConfig: {
-        record_id: record.id
-      },
-      callback: function(w) {
-        w.show();
-        w.on('close', function() {
-          if (w.closeRes === "ok") {
-            this.netzkeReloadStore();
-          }
-        }, this);
-      }
-    });
-  },
+	// HACK to work around buffered store's buggy reload()
+	if (!store.lastRequestStart) {
+	    store.load(opts);
+	} else store.reload(opts);
+    },
 
-  // always reset store to first page on reload
-  // to avoid load bug when moving from a higher page count
-  // to a grid with a lower page count
-  reload: function(opts = {
-    start: 0
-  }) {
-    this.netzkeReloadStore(opts);
-  },
+    doViewInForm: function(record) {
+	this.netzkeLoadComponent("view_window", {
+	    serverConfig: {
+		record_id: record.id
+	    },
+	    callback: function(w) {
+		w.show();
+		w.on('close', function() {
+		    if (w.closeRes === "ok") {
+			this.netzkeReloadStore();
+		    }
+		}, this);
+	    }
+	});
+    },
 
-  reloadAll: function() {
-    var me = this;
-    var children = me.serverConfig.child_components || [];
-    this.store.reload();
-    for (child of children) {
-      var comp = me.findComponent(child);
-      if (comp && comp.reload) {
-        comp.reload()
-      }
-    }
-  },
+    // always reset store to first page on reload
+    // to avoid load bug when moving from a higher page count
+    // to a grid with a lower page count
+    reload: function(opts = {
+	start: 0
+    }) {
+	this.netzkeReloadStore(opts);
+    },
 
-  clearFilters: function() {
-    this.filters.clearFilters();
-  },
+    reloadAll: function() {
+	var me = this;
+	var children = me.serverConfig.child_components || [];
+	this.store.reload();
+	for (child of children) {
+	    var comp = me.findComponent(child);
+	    if (comp && comp.reload) {
+		comp.reload()
+	    }
+	}
+    },
+
+    clearFilters: function() {
+	this.filters.clearFilters();
+    },
 }
