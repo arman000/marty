c8bad581262f86b7979584161c957c3f4453dec6
Agrim Pathak <agrim.pathak@pnmac.com>
2019-08-09 16:41:16 -0700

    RpcCall#marty_download read_timeout parameter

--- lib/marty/rpc_call.rb
+++ lib/marty/rpc_call.rb
@@ -24,12 +24,16 @@ class Marty::RpcCall
     res
   end
 
-  def self.marty_download(host, port, path, job_id, ssl = false)
+  def self.marty_download(
+    host, port, path, job_id, ssl = false, read_timeout = 60)
+
     params = { job_id: job_id }
     url = path + '?' + URI.encode(URI.encode_www_form(params))
 
     http = Net::HTTP.new(host, port)
     http.use_ssl = ssl
+    http.read_timeout = read_timeout
+
     request = Net::HTTP::Get.new(url)
 
     begin
