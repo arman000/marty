ad5880d1e2ce35193f4ec657982af6e3805d301f
eugene <ezvy@yandex.ru>
2019-08-22 16:25:13 +0300

    Add diag for scheduled_jobs

---
+++ other/marty/diagnostic/scheduled_jobs.rb
@@ -0,0 +1,22 @@
+module Marty::Diagnostic; class ScheduledJobs < Base
+  self.aggregatable = false
+
+  diagnostic_fn do
+    logs = ::Marty::BackgroundJob::Log.order(job_class: :asc, status: :desc, id: :desc).
+      select('DISTINCT ON(job_class, status) *').first(1000)
+
+    failed_total = ::Marty::BackgroundJob::Log.where(status: :failure).count
+
+    result = logs.each_with_object({}) do |log, hash|
+      message = "Status: #{log.status}, last_run: #{log.created_at}"
+      message = "#{message}, error: #{log.error}" if log.failure?
+      hash[log.job_class] = log.success? ? message : error(message)
+    end
+
+    result['Failures total'] = 0
+    result['Failures total'] = error(failed_total) if failed_total != 0
+
+    result
+  end
+end
+end
