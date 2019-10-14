4c9ce5b9beeae484fbbb103e465d76f7e7cc9052
eugene <ezvy@yandex.ru>
2019-10-14 16:25:54 +0300

    171890 - Add a report for enum values in DB.

---
+++ app/services/marty/enums/report.rb
@@ -0,0 +1,18 @@
+module Marty
+  module Enums
+    module Report
+      extend Delorean::Functions
+
+      delorean_fn :call do
+        ActiveRecord::Base.connection.execute(
+          'SELECT
+          t.typname AS enum_name,
+          e.enumlabel AS value
+          FROM pg_type t
+          JOIN pg_enum e ON t.oid = e.enumtypid
+          JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace'
+        ).to_a
+      end
+    end
+  end
+end
