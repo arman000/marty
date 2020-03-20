ed3009b7c5c38358821ab49dd8e6e1c6f3daf902
John Rhoads <john.rhoads@pnmac.com>
2020-03-20 14:23:40 -0700

    why?

--- db/migrate/526_add_schedule_id_to_delayed_jobs.rb
+++ db/migrate/526_add_schedule_id_to_delayed_jobs.rb
@@ -4,11 +4,37 @@ class AddScheduleIdToDelayedJobs < ActiveRecord::Migration[5.1]
     add_index :delayed_jobs, :schedule_id
 
     reversible do |dir|
-      dir.up { set_ids }
+      dir.up do
+        set_ids
+        reschedule_jobs
+      end
     end
   end
 
   def set_ids
     ::Marty::Jobs::Schedule.call
   end
+
+  def reschedule_jobs
+    Marty::BackgroundJob::Schedule.all.each do |job|
+      puts job.attributes
+      Marty::BackgroundJob::Schedule.where(
+          job_class: job[:job_class]
+        ).destroy_all
+
+      if class_exists?(job[:job_class])
+        new_job = Marty::BackgroundJob::Schedule.create!(job.attributes)
+        "::#{job[:job_class]}".constantize.schedule(schedule_obj: new_job)
+      end
+
+    end
+  end
+
+  def class_exists?(class_name)
+    klass = Module.const_get(class_name)
+    return klass.is_a?(Class)
+  rescue NameError
+    return false
+  end
+
 end
