281b5912236720aa9ffa4e25e7306806d607d4ad
John Rhoads <john.rhoads@pnmac.com>
2020-03-20 14:14:45 -0700

    all attrib into create!

--- db/migrate/527_reschedule_all_jobs.rb
+++ db/migrate/527_reschedule_all_jobs.rb
@@ -9,13 +9,7 @@ class RescheduleAllJobs < ActiveRecord::Migration[5.1]
         ).destroy_all
 
       if class_exists?(job[:job_class])
-
-        new_job = Marty::BackgroundJob::Schedule.create!(
-            job_class: job[:job_class],
-            cron: job[:cron],
-            state: job[:state]
-          )
-
+        new_job = Marty::BackgroundJob::Schedule.create!(job.attributes)
         "::#{job[:job_class]}".constantize.schedule(schedule_obj: new_job)
       end
 
