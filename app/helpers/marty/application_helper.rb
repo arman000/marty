ca2f471091ae3a92f279d6c34d02f932748ca8c1
Hayden McFarland <hayden.mcfarland@pnmac.com>
2020-04-08 15:27:57 -0700

    allow rails root application to import 'application.js' from marty default template and enable dark mode default config

--- app/helpers/marty/application_helper.rb
+++ app/helpers/marty/application_helper.rb
@@ -1,4 +1,12 @@
 module Marty
   module ApplicationHelper
+    DEFAULT_JS_PATH = 'app/assets/javascripts'
+    def javascript_exists?(file, file_extension = 'js')
+      path = Rails.configuration.marty.assets_javascripts_path ||
+             DEFAULT_JS_PATH
+
+      asset_path = Rails.root.join("#{path}/#{file}.#{file_extension}")
+      File.exist?(asset_path)
+    end
   end
 end
--- lib/marty/version.rb
+++ lib/marty/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Marty
-  VERSION = '11.1.0'
+  VERSION = '11.1.1'
 end
