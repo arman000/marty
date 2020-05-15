dd92b1d420148ba83610fcf0eb4718690b198c4c
Omri Gabay <omri.gabay@pnmac.com>
2020-05-15 11:59:51 -0700

    Prettier fix manifest.js

--- app/assets/config/marty/manifest.js
+++ app/assets/config/marty/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 
-
 // Images and fonts so that views can link to them
 //= link_tree ../fonts
 //= link_tree ../images
