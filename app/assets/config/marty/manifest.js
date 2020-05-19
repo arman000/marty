3e7b65a873b87c8d8affa660b838320c3b2e252c
Omri Gabay <omri.gabay@pnmac.com>
2020-05-19 12:29:21 -0700

    This won't work but meh

--- app/assets/config/marty/manifest.js
+++ app/assets/config/marty/manifest.js
@@ -1,7 +1,7 @@
 // JS and CSS bundles
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link_directory ../../javascripts/marty .js
+//= link_directory ../../stylesheets/marty .css
 
 // Images and fonts so that views can link to them
-//= link_tree ../fonts
-//= link_tree ../images
+//= link_tree ../fonts/
+//= link_tree ../images/
