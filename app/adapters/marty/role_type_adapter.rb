b451246d93d0bea109c701b045e5f6ba1dc7ef6e
Hayden McFarland <hayden.mcfarland@pnmac.com>
2020-07-06 16:08:40 -0700

    refactor

--- app/adapters/marty/role_type_adapter.rb
+++ app/adapters/marty/role_type_adapter.rb
@@ -2,7 +2,12 @@ module Marty
   module RoleTypeAdapter
     mattr_accessor :klass, default: ::Marty::RoleType
     class << self
-      [:values, :from_nice_names, :to_nice_names].each do |klass_method|
+      [
+        :get_all,
+        :from_nice_names,
+        :to_nice_names,
+        :values
+      ].each do |klass_method|
         delegate klass_method, to: :klass
       end
     end
