ff72cb980711b8a0c133ab7e98901e5396cd073b
John Rhoads <john.rhoads@pnmac.com>
2020-08-10 11:45:47 -0400

    fix typo

--- app/jobs/marty/delayed_job_worker_reaper_job.rb
+++ app/jobs/marty/delayed_job_worker_reaper_job.rb
@@ -6,7 +6,7 @@ module Marty
       current_node = Marty::Diagnostic::Node.my_ip
       Marty::Logger.info(LOG_PREFIX, "running on #{current_node}")
       res = `#{Marty::MainAuthApp.new.bg_command('restart')}`
-      Marty::Logger.info(LOG_PREFIX"#{current_node}: #{res}")
+      Marty::Logger.info(LOG_PREFIX, "#{current_node}: #{res}")
     end
   end
 end
