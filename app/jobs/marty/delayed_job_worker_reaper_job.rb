b798395583161485cc9bb05b5e8a66573b03db6c
John Rhoads <john.rhoads@pnmac.com>
2020-08-10 10:43:56 -0400

    const and rubocop

--- app/jobs/marty/delayed_job_worker_reaper_job.rb
+++ app/jobs/marty/delayed_job_worker_reaper_job.rb
@@ -1,13 +1,18 @@
 module Marty
   class DelayedJobWorkerReaperJob < ::Marty::CronJob
-    LOG_PREFIX = 'DelayedJobWorkerReaperJob'
     def perform
-      system_login = Rails.configuration.marty.system_account
-
       current_node = Marty::Diagnostic::Node.my_ip
-      Marty::Logger.info(LOG_PREFIX, "running on #{current_node}")
+      Marty::Logger.info(
+        'DelayedJobWorkerReaperJob',
+        "running on #{current_node}",
+      )
+
       res = `#{Marty::MainAuthApp.new.bg_command('restart')}`
-      Marty::Logger.info(LOG_PREFIX, "#{current_node}: #{res}")
+
+      Marty::Logger.info(
+        'DelayedJobWorkerReaperJob',
+        "#{current_node}: #{res}",
+      )
     end
   end
 end
