601ec58837a28888129f4b90b3624bf17695ff76
John Rhoads <john.rhoads@pnmac.com>
2020-12-31 09:59:24 -0800

    Update sql_servers.rb

--- config/initializers/sql_servers.rb
+++ config/initializers/sql_servers.rb
@@ -1,5 +1,5 @@
 if File.exist?('config/sql_servers.yml')
   require 'marty/sql_servers'
 
-  Marty::SqlServers.generate_database_connections!(eager_start: Rails.env.production?)
+  Marty::SqlServers.generate_database_connections!
 end
